596387cdb9991902945a512098b12586ba477bcd
Edwin <edwinlee168@outlook.com>
2018-01-18 14:53:41 +1100

    auth improve

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,47 +1,88 @@
+import { ApiService } from './../../services/api.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import * as moment from 'moment';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
+
 @Injectable()
 export class AuthService {
+
 
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string ) {
-    return this.http.post('http://app.eiz.com.au/auth', {username: username, password: password}, {}).map(data => {
-        if (data['ack']){
-          localStorage.setItem('token', data['data']['token']);
-          return data['data']['token'];
-        }
-        return false;
+
+    return this.http.post('http://app.eiz.com.au/auth/auth', { email: username, password: password }, {} ).map(data => {
+      if (data['ack']) {
+        this.setSession(data['data']);
+        return true;
+      }
+      return false;
     });
   }
 
   private setSession(authResult) {
-    const expiresAt = moment().add(authResult.expiresIn, 'second');
-
-    localStorage.setItem('id_token', authResult.idToken);
+    const expiresAt = moment().add(authResult['expires_at'], 'second');
+    const activeAt = moment().add(authResult['refresh_expires_at'] , 'second');
+    localStorage.setItem('token', authResult['token']);
+    localStorage.setItem('refresh_expires_at', JSON.stringify(activeAt.valueOf()) );
     localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()) );
   }
 
   logout() {
     localStorage.removeItem('token');
+    localStorage.removeItem('refresh_expires_at');
     localStorage.removeItem('expires_at');
   }
 
   public isLoggedIn() {
-      return moment().isBefore(this.getExpiration());
+    if(localStorage.getItem('token')){
+      if (moment().isBefore(this.getExpiration())){
+        return true;
+      }
+    }
+    return false;
   }
 
-  isLoggedOut() {
+  public isLoggedOut() {
       return !this.isLoggedIn();
   }
 
-  getExpiration() {
-      const expiration = localStorage.getItem('expires_at');
-      const expiresAt = JSON.parse(expiration);
-      return moment(expiresAt);
+  private getStorageTime(itemName: string){
+    const item = JSON.parse(localStorage.getItem(itemName));
+    return moment(item);
   }
+  getExpiration() {
+      return this.getStorageTime('expires_at');
+  }
+
+  getActive() {
+    return this.getStorageTime('refresh_expires_at');
+  }
+  isActive() {
+    const activeTime = this.getActive();
+
+    if (moment().isBefore(activeTime)) {
+      return true;
+    }else{
+      return false;
+    }
+
+  }
+
+  renewToken(){
+    let newActive = moment().add(1, 'm');
+    if (newActive.isBefore(this.getExpiration())){
+      localStorage.removeItem('refresh_expires_at');
+      localStorage.addItem('refresh_expires_at', newActive );
+
+      return true;
+    }else{
+      this.logout();
+      return false;
+    }
+  }
+
 }
